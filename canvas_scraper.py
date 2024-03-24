b399da4b6ee08eb2d26e351c87a1ab67343c4bc9
Mortbros <sandromort@gmail.com>
2024-03-25 00:45:24 +1100

    Add sign in sequence to canvas scraper

--- canvas_scraper.py
+++ canvas_scraper.py
@@ -1,12 +1,25 @@
-from web_scraper import WebScraper, Element
+from web_scraper import WebScraper, Element, ElementSequence, Selector
 
 
-user_id = Element(name="User id field", selectors=[], action="click")
+
+username_textbox = Element(name="Username textbox", selectors="#Ecom_User_ID", action="send", send_values="s4003020")
+with open("C:\\Users\\sandr\\OneDrive\\Programming\\Selenium\\test.txt", 'r') as f:
+    output = f.read()
+password_textbox = Element(name="Password textbox", selectors="#Ecom_Password", action="send", send_values=output)
+login_button = Element(name="Login button", selectors="#loginButton2", action="click")
+
+
+sign_in_sequence = ElementSequence(elements=[username_textbox, password_textbox, login_button])
 
 AA_scraper = WebScraper(
     name="Algorithms and analysis",
     url="https://rmit.instructure.com/courses/125079/pages/week-1-overview-read-me-first?module_item_id=5924235",
     sign_in_url="https://rmit.instructure.com/",
+    sequences=sign_in_sequence,
+    sign_in_sequence=sign_in_sequence
 )
 
-AA_scraper.start()
+AA_scraper.sign_in()
+
+
+# AA_scraper.start()
