68127aee30d0d7c1981d03c0888b6b05d065d14f
Mortbros <sandromort@gmail.com>
2024-03-26 05:16:56 +1100

    Add more selectors to next_button, combine start sequence into single run function

--- canvas_scraper.py
+++ canvas_scraper.py
@@ -21,7 +21,8 @@ content = Element(
 
 next_button = Element(
     name="Next button",
-    selectors=['//*[@id="module_navigation_target"]/div/div[2]/div/span[2]/a', '//*[@id="sequence_footer"]/div[2]/div/span[2]', '//*[@id="module_navigation_target"]/div/div[2]/div/span[2]', '//*[@id="module_sequence_footer"]/div[2]/div/span[2]', '/html/body/div[4]/div[2]/div[2]/div[3]/div[1]/div/div[2]/div/div[2]/div/span[2]', '/html/body/div[4]/div[2]/div[2]/div[3]/div[1]/div/div[5]/div[2]/div/span[2]'],
+    
+    selectors=['//*[@id="module_navigation_target"]/div/div[2]/div/span[2]/a', '//*[@id="sequence_footer"]/div[2]/div/span[2]', '//*[@id="sequence_footer"]/div[2]/div/span[2]/a', '//*[@id="module_navigation_target"]/div/div[2]/div/span[2]', '//*[@id="module_sequence_footer"]/div[2]/div/span[2]', '/html/body/div[4]/div[2]/div[2]/div[3]/div[1]/div/div[2]/div/div[2]/div/span[2]', '/html/body/div[4]/div[2]/div[2]/div[3]/div[1]/div/div[5]/div[2]/div/span[2]'],
     action="click"
 )
 
@@ -35,12 +36,9 @@ def canvas_scraper(name, url):
         sequences=grab_content_sequence,
         sign_in_sequence=sign_in_sequence
     )
-    scraper.sign_in()
-    scraper.start()
-    scraper.iterate_sequence()
-    scraper.write()
+    scraper.run()
 
 
-canvas_scraper("Algorithms and Analysis", "https://rmit.instructure.com/courses/125079/pages/week-1-overview-read-me-first?module_item_id=5924235")
-canvas_scraper("Fullstack Development", "https://rmit.instructure.com/courses/132457/pages/welcome-to-the-course?module_item_id=5888855")
+# canvas_scraper("Algorithms and Analysis", "https://rmit.instructure.com/courses/125079/pages/week-1-overview-read-me-first?module_item_id=5924235")
+# canvas_scraper("Fullstack Development", "https://rmit.instructure.com/courses/132457/pages/welcome-to-the-course?module_item_id=5888855")
 canvas_scraper("Software Engineering Fundamentals", "https://rmit.instructure.com/courses/124890/pages/our-commitment-to-reconciliation?module_item_id=6024511")
