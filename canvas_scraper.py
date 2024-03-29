89a9aec356a897cc4a1d32ff1a20d5cdf50f8f11
Mortbros <sandromort@gmail.com>
2024-03-29 15:14:12 +1100

    Format with black

--- canvas_scraper.py
+++ canvas_scraper.py
@@ -1,32 +1,75 @@
 from web_scraper import WebScraper, Element, ElementSequence, Selector
 
 # Not the biggest fan of the critical=False for this. It is there because of the ensure_absensce in the elementSequence
-username_textbox = Element(name="Username textbox", selectors="#Ecom_User_ID", send_values="s4003020", critical=False, timeout=1)
-with open("C:\\Users\\sandr\\OneDrive\\Programming\\Selenium\\test.txt", 'r') as f:
+username_textbox = Element(
+    name="Username textbox",
+    selectors="#Ecom_User_ID",
+    send_values="s4003020",
+    critical=False,
+    timeout=1,
+)
+with open("C:\\Users\\sandr\\OneDrive\\Programming\\Selenium\\test.txt", "r") as f:
     output = f.read()
-password_textbox = Element(name="Password textbox", selectors="#Ecom_Password", send_values=output, critical=False, timeout=1)
-login_button = Element(name="Login button", selectors="#loginButton2", click=True, critical=False, timeout=1)
-absent_login_button = Element(name="Ensure absence login button", selectors="#loginButton2", ensure_absence=True, timeout=1)
+password_textbox = Element(
+    name="Password textbox",
+    selectors="#Ecom_Password",
+    send_values=output,
+    critical=False,
+    timeout=1,
+)
+login_button = Element(
+    name="Login button",
+    selectors="#loginButton2",
+    click=True,
+    critical=False,
+    timeout=1,
+)
+absent_login_button = Element(
+    name="Ensure absence login button",
+    selectors="#loginButton2",
+    ensure_absence=True,
+    timeout=1,
+)
 
 
-sign_in_sequence = ElementSequence(name="Sign In", elements=[username_textbox, password_textbox, login_button, absent_login_button], restart_on_fail=5)
+sign_in_sequence = ElementSequence(
+    name="Sign In",
+    elements=[username_textbox, password_textbox, login_button, absent_login_button],
+    restart_on_fail=5,
+)
 
 
 content = Element(
     name="Content",
-    selectors=['//*[@id="wiki_page_show"]', '//*[@id="content"]', '/html/body/div[4]/div[2]/div[2]/div[3]/div[1]/div/div[1]', '/html/body/div[4]/div[2]/div[2]/div[3]/div[1]/div'],
-    capture_attribute="innerHTML"
+    selectors=[
+        '//*[@id="wiki_page_show"]',
+        '//*[@id="content"]',
+        "/html/body/div[4]/div[2]/div[2]/div[3]/div[1]/div/div[1]",
+        "/html/body/div[4]/div[2]/div[2]/div[3]/div[1]/div",
+    ],
+    capture_attribute="innerHTML",
 )
 
 next_button = Element(
     name="Next button",
-    selectors=['//*[@id="sequence_footer"]/div[2]/div/span[2]', '//*[@id="module_navigation_target"]/div/div[2]/div/span[2]/a', '//*[@id="module_navigation_target"]/div/div[2]/div/span/a', '//*[@id="sequence_footer"]/div[2]/div/span[2]/a', '//*[@id="module_navigation_target"]/div/div[2]/div/span[2]', '//*[@id="module_sequence_footer"]/div[2]/div/span[2]', '/html/body/div[4]/div[2]/div[2]/div[3]/div[1]/div/div[2]/div/div[2]/div/span[2]', '/html/body/div[4]/div[2]/div[2]/div[3]/div[1]/div/div[5]/div[2]/div/span[2]'],
+    selectors=[
+        '//*[@id="sequence_footer"]/div[2]/div/span[2]',
+        '//*[@id="module_navigation_target"]/div/div[2]/div/span[2]/a',
+        '//*[@id="module_navigation_target"]/div/div[2]/div/span/a',
+        '//*[@id="sequence_footer"]/div[2]/div/span[2]/a',
+        '//*[@id="module_navigation_target"]/div/div[2]/div/span[2]',
+        '//*[@id="module_sequence_footer"]/div[2]/div/span[2]',
+        "/html/body/div[4]/div[2]/div[2]/div[3]/div[1]/div/div[2]/div/div[2]/div/span[2]",
+        "/html/body/div[4]/div[2]/div[2]/div[3]/div[1]/div/div[5]/div[2]/div/span[2]",
+    ],
     click=True,
-    # pageload_wait=5,
-    return_on_click=True
+    return_on_click=True,
 )
 
-grab_content_sequence = ElementSequence(name="Grab canvas content", elements=[content, next_button], run_until_fail=True)
+grab_content_sequence = ElementSequence(
+    name="Grab canvas content", elements=[content, next_button], run_until_fail=True
+)
+
 
 def canvas_scraper(name, url):
     scraper = WebScraper(
@@ -34,11 +77,21 @@ def canvas_scraper(name, url):
         url=url,
         sign_in_url="https://rmit.instructure.com/",
         sequences=grab_content_sequence,
-        sign_in_sequence=sign_in_sequence
+        sign_in_sequence=sign_in_sequence,
     )
     scraper.run()
 
 
-canvas_scraper("Algorithms and Analysis", "https://rmit.instructure.com/courses/125079/pages/week-1-overview-read-me-first?module_item_id=5924235")
-canvas_scraper("Fullstack Development", "https://rmit.instructure.com/courses/132457/pages/welcome-to-the-course?module_item_id=5888855")
-canvas_scraper("Software Engineering Fundamentals", "https://rmit.instructure.com/courses/124890/pages/our-commitment-to-reconciliation?module_item_id=6024511")
+for i in range(5):
+    canvas_scraper(
+        "Algorithms and Analysis",
+        "https://rmit.instructure.com/courses/125079/pages/week-1-overview-read-me-first?module_item_id=5924235",
+    )
+    canvas_scraper(
+        "Fullstack Development",
+        "https://rmit.instructure.com/courses/132457/pages/welcome-to-the-course?module_item_id=5888855",
+    )
+    canvas_scraper(
+        "Software Engineering Fundamentals",
+        "https://rmit.instructure.com/courses/124890/pages/our-commitment-to-reconciliation?module_item_id=6024511",
+    )
--- web_scraper.py
+++ web_scraper.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support import expected_conditions as EC
+
 # from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support.wait import WebDriverWait
 
@@ -19,11 +20,11 @@ import typing
 
 # TODO: download all videos, grab transcript, add to HTML
 # TODO: open iframe and grab content from it
-    # iframe_capture="all"|"extract body"|[ElementSequence]
-    # run a sequence if a specific condition is met in the URL
-    # Or just grab entire HTML and append
-    # Or run a list of elementSequences until HTML is extracted, else just grab entire HTML
-    # OPTIONAL: Flag for recursive iframe html extraction
+# iframe_capture="all"|"extract body"|[ElementSequence]
+# run a sequence if a specific condition is met in the URL
+# Or just grab entire HTML and append
+# Or run a list of elementSequences until HTML is extracted, else just grab entire HTML
+# OPTIONAL: Flag for recursive iframe html extraction
 # TODO: status row
 # TODO: get transcript of all youtube videos https://pypi.org/project/youtube-transcript-api/
 # TODO: add support for regular expression matching in element url field so it only works on urls that match the regex
@@ -43,13 +44,15 @@ ELEMENT_FIND_MODES = ["run for n time", "wait per selector"]
 IGNORED_EXCEPTIONS = (NoSuchElementException, StaleElementReferenceException)
 
 
-
 class ElementExistsError(Exception):
     """Raised if the element exists when it shouldn't"""
+
     pass
+
 
 class ElementNotFoundError(Exception):
     """Raised if the element doesn't exist when it should"""
+
     pass
 
 
@@ -68,30 +71,49 @@ def ordinal_suffix(i):
         return "rd"
     else:
         return "th"
-    
 
 
 class WebScraper:
-    def __init__(self, name, url, sign_in_url, sequences, sign_in_sequence, headless=False, unique_file_name=True):
+    def __init__(
+        self,
+        name,
+        url,
+        sign_in_url,
+        sequences,
+        sign_in_sequence,
+        headless=False,
+        unique_file_name=True,
+    ):
         self.name = name
         self.url = url
-        self.status_row = {"iter_num": 0, "action": "Adding body to string", "status": None, "ID": None}
+        self.status_row = {
+            "iter_num": 0,
+            "action": "Adding body to string",
+            "status": None,
+            "ID": None,
+        }
         self.sign_in_url = sign_in_url
-        
+
         if isinstance(sequences, list):
             if all(isinstance(i, ElementSequence) for i in sequences):
                 self.sequences = sequences
             else:
-                raise Exception("Invalid list of element sequence, all elements must be of type ElementSequence")
+                raise Exception(
+                    "Invalid list of element sequence, all elements must be of type ElementSequence"
+                )
         elif isinstance(sequences, ElementSequence):
             self.sequences = [sequences]
         else:
-            raise Exception(f"Invalid list of element sequences, must be of type list (currently {type(sequences)})")
+            raise Exception(
+                f"Invalid list of element sequences, must be of type list (currently {type(sequences)})"
+            )
 
         if isinstance(sequences, ElementSequence):
             self.sign_in_sequence = sign_in_sequence
         else:
-            raise Exception(f"Invalid sign in element sequence, must be of ElementSequence (currently {type(sequences)})")
+            raise Exception(
+                f"Invalid sign in element sequence, must be of ElementSequence (currently {type(sequences)})"
+            )
 
         self.options = Options()
         self.options.headless = headless
@@ -116,16 +138,22 @@ class WebScraper:
             self.write()
 
     def write(self):
-        open(f"{self.name}{datetime.now().strftime(' %Y-%m-%d_%H-%M-%S') if self.unique_file_name else ''}.html", "w", encoding="utf-8").write(self.html)
+        open(
+            f"{self.name}{datetime.now().strftime(' %Y-%m-%d_%H-%M-%S') if self.unique_file_name else ''}.html",
+            "w",
+            encoding="utf-8",
+        ).write(self.html)
 
     # TODO: rename this function or combine it with something else
     def open_url(self):
         print(f"Starting scraper: {self.name}")
         self.driver.get(self.url)
 
-    def iterate_sequence(self, iterations: int=1):
+    def iterate_sequence(self, iterations: int = 1):
         if len(self.sequences) < self.sequence_index + iterations:
-            raise Exception(f"Invalid iteration, list is of length {len(self.sequences)}, iteration would access index {self.sequence_index + iterations + 1}")
+            raise Exception(
+                f"Invalid iteration, list is of length {len(self.sequences)}, iteration would access index {self.sequence_index + iterations + 1}"
+            )
         for i in range(self.sequence_index, self.sequence_index + iterations):
             print(f"Running {self.name} scraper sequence {i}:")
             self.html += self.sequences[i].run(driver=self.driver)
@@ -135,7 +163,7 @@ class WebScraper:
         print(f"Signing into scraper: {self.name}")
         self.driver.get(self.sign_in_url)
         self.sign_in_sequence.run(driver=self.driver)
-        
+
 
 # class StatusRow:
 #     def __init__(self, status_row = {"iter_num": 0, "action": "Adding body to string", "status": None, "ID": None}):
@@ -151,9 +179,17 @@ class WebScraper:
 
 
 class ElementSequence:
-    def __init__(self, name, elements=[], url=None, run_until_fail=False, restart_on_fail: int=1, restart_delay=RESTART_DELAY):
+    def __init__(
+        self,
+        name,
+        elements=[],
+        url=None,
+        run_until_fail=False,
+        restart_on_fail: int = 1,
+        restart_delay=RESTART_DELAY,
+    ):
         self.html = ""
-        
+
         self.name = name
 
         if isinstance(elements, list):
@@ -162,10 +198,14 @@ class ElementSequence:
             elif all(isinstance(i, Element) for i in elements):
                 self.elements = elements
             else:
-                raise Exception("Invalid element sequence, all elements must be of type Element")
+                raise Exception(
+                    "Invalid element sequence, all elements must be of type Element"
+                )
         else:
-            raise Exception(f"Invalid element sequence must be of type list (currently {type(elements)})")
-        
+            raise Exception(
+                f"Invalid element sequence must be of type list (currently {type(elements)})"
+            )
+
         self.elements = elements
         self.url = url
 
@@ -183,16 +223,16 @@ class ElementSequence:
         else:
             if isinstance(element, Element):
                 self.elements.append(element)
-    
+
     def __sub__(self, element):
         if element in self.elements:
             self.elements.remove(element)
         # else:
         #     raise Exception("Element {element} not in sequence")
-    
+
     # Runs iteration
     def run(self, driver):
-        # This check is located here because an ElementSequence can be initialised with no elements, then populated later 
+        # This check is located here because an ElementSequence can be initialised with no elements, then populated later
         if len(self.elements) != 0:
             # TODO: i'm not the biggest fan of moving over a block of code to the _excecute_iteration private function
             if self.run_until_fail:
@@ -208,13 +248,15 @@ class ElementSequence:
                     if self._excecute_iteration(driver):
                         break
                 else:
-                    raise Exception(f"Element sequence \"{self.name}\" failed to find and validate after {self.restart_on_fail} attempts")
-            
+                    raise Exception(
+                        f'Element sequence "{self.name}" failed to find and validate after {self.restart_on_fail} attempts'
+                    )
+
             print("\t\tFinished after failing to find")
         else:
             raise Exception("Cannot run empty ElementSequence")
         return self.html
-    
+
     def _excecute_iteration(self, driver):
         if self.url:
             driver.get(self.url)
@@ -232,11 +274,28 @@ class ElementSequence:
                 return False
             else:
                 raise ElementNotFoundError(e)
-        
+
 
 # An object representing an element, including all possible methods of locating it (xpath, css selector)
 class Element:
-    def __init__(self, name, selectors=[], mode="run for n time", capture_attribute="", click=False, return_on_click=False, send_values=None, clear=True, ensure_absence=False, critical=True, timeout=DEFAULT_TIMEOUT, pageload_wait=0, url=None, iframe_capture="", retry_on_stale: int=5):
+    def __init__(
+        self,
+        name,
+        selectors=[],
+        mode="run for n time",
+        capture_attribute="",
+        click=False,
+        return_on_click=False,
+        send_values=None,
+        clear=True,
+        ensure_absence=False,
+        critical=True,
+        timeout=DEFAULT_TIMEOUT,
+        pageload_wait=0,
+        url=None,
+        iframe_capture="",
+        retry_on_stale: int = 5,
+    ):
         self.name = name
 
         if mode not in ELEMENT_FIND_MODES:
@@ -263,7 +322,7 @@ class Element:
                     raise Exception(f"Invalid selector type '{type(s)}'")
         else:
             raise Exception(f"Invalid selector type '{type(selectors)}'")
-        
+
         self.capture_attribute = capture_attribute
         self.click = click
         self.return_on_click = return_on_click
@@ -282,7 +341,7 @@ class Element:
         # the element's selector will succeed in matching, but it won't be what we are looking for
         self.pageload_wait = pageload_wait
         self._last_url = None
-        
+
         self.url = url
 
         # Brackets for readability
@@ -290,12 +349,12 @@ class Element:
             self.iframe_capture = iframe_capture
         else:
             raise Exception("Invalid iframe capture method")
-        
+
         self.retry_on_stale = retry_on_stale
         self._current_retry_on_stale = retry_on_stale
 
         # self.status_row = StatusRow()
-        
+
     def run(self, driver):
         out = None
         current_url = driver.current_url
@@ -303,45 +362,54 @@ class Element:
             print(f"\t\tWaiting for pageload ({self.pageload_wait}s)")
             self._last_url = current_url
             time.sleep(self.pageload_wait)
-        
+
         if self.mode == ELEMENT_FIND_MODES[0]:
             self.timeout = 0
             end_time = time.time() + self._loop_timeout
             while time.time() < end_time and not out:
                 out = self.find(driver)
                 self._current_retry_on_stale = self.retry_on_stale
-        
+
         elif self.mode == ELEMENT_FIND_MODES[1]:
             out = self.find(driver)
 
         self._current_retry_on_stale = self.retry_on_stale
 
         return out
-    
-    
+
     def find(self, driver):
-        
+
         attribute_catches = []
         return_status = False
 
         for i, selector in enumerate(self.selectors):
             print(f"\t\tFinding element: {self.name}, selector {selector} ")
-            
+
             try:
                 elements = []
                 try:
-                    elements = WebDriverWait(driver, self.timeout, ignored_exceptions=IGNORED_EXCEPTIONS).until(EC.presence_of_all_elements_located((selector.sel_type, selector.sel_value)))
+                    elements = WebDriverWait(
+                        driver, self.timeout, ignored_exceptions=IGNORED_EXCEPTIONS
+                    ).until(
+                        EC.presence_of_all_elements_located(
+                            (selector.sel_type, selector.sel_value)
+                        )
+                    )
                 except TimeoutException:
-                    print(f"\t\t\tFailed to find element with {i + 1}{ordinal_suffix(i + 1)} selector")
+                    print(
+                        f"\t\t\tFailed to find element with {i + 1}{ordinal_suffix(i + 1)} selector"
+                    )
 
                 if len(elements) > 0:
                     print("\t\t\tSuccess: Element found")
                     return_status = True
-                
+
                 for element in elements:
                     if self.ensure_absence:
-                        raise ElementExistsError(f"Element '{self.name}' found when it shouldn't exist, selector '{selector}'")
-                    
+                        raise ElementExistsError(
+                            f"Element '{self.name}' found when it shouldn't exist, selector '{selector}'"
+                        )
+
                     if self.click:
                         element.click()
                         if self.return_on_click:
@@ -350,8 +418,7 @@ class Element:
                         if self.clear:
                             element.clear()
                         element.send_keys(self.send_values)
-                    
-                        
+
                     if self.capture_attribute:
                         attribute_value = element.get_attribute(self.capture_attribute)
                         if self.capture_attribute == "innerHTML":
@@ -360,36 +427,55 @@ class Element:
                             attribute_catches += attribute_value
                         else:
                             attribute_catches.append(attribute_value)
-                
+
                 # TODO: untested
                 if self.iframe_capture:
                     # This is probably bad practice, but it works(?)
 
-                    IFRAME_ELEMENT_SRC = Element(name="(Internal) Iframe capture URL source", selectors=Selector("iframe", By.TAG_NAME), timeout=5, capture_attribute="src", critical=False)
+                    IFRAME_ELEMENT_SRC = Element(
+                        name="(Internal) Iframe capture URL source",
+                        selectors=Selector("iframe", By.TAG_NAME),
+                        timeout=5,
+                        capture_attribute="src",
+                        critical=False,
+                    )
 
                     iframe_srcs = IFRAME_ELEMENT_SRC.run(driver)
                     if iframe_srcs:
-                        
-                        BODY_ELEMENT_HTML = Element(name="(Internal) Iframe capture innerHTML", selectors=Selector("body", By.TAG_NAME), iframe_capture="body", timeout=5, capture_attribute="innerHTML", critical=False)
-                        
+
+                        BODY_ELEMENT_HTML = Element(
+                            name="(Internal) Iframe capture innerHTML",
+                            selectors=Selector("body", By.TAG_NAME),
+                            iframe_capture="body",
+                            timeout=5,
+                            capture_attribute="innerHTML",
+                            critical=False,
+                        )
+
                         for iframe_src in iframe_srcs:
                             # TODO: mac support with Keys.COMMAND?
-                            driver.find_element_by_tag_name('body').send_keys(Keys.CONTROL + 't')
+                            driver.find_element_by_tag_name("body").send_keys(
+                                Keys.CONTROL + "t"
+                            )
 
                             driver.get(iframe_src)
                             if new_tab_html := BODY_ELEMENT_HTML.run(driver):
                                 attribute_catches = attribute_catches + new_tab_html
 
-                            driver.find_element_by_tag_name('body').send_keys(Keys.CONTROL + 'w') 
-        
+                            driver.find_element_by_tag_name("body").send_keys(
+                                Keys.CONTROL + "w"
+                            )
+
             except StaleElementReferenceException as e:
                 if self._current_retry_on_stale <= 0:
                     raise e
                 else:
-                    print(f"\t\t\tRetrying due to stale element ({self._current_retry_on_stale} tr{'ies' if self._current_retry_on_stale > 1 else 'y'} left)")
+                    print(
+                        f"\t\t\tRetrying due to stale element ({self._current_retry_on_stale} tr{'ies' if self._current_retry_on_stale > 1 else 'y'} left)"
+                    )
                     self._current_retry_on_stale -= 1
                     self.run(driver)
-            
+
         if self.ensure_absence:  # exit if the element must appear
             print("\t\t\tSuccess: Element not found (yes really)")
             return True
@@ -408,10 +494,10 @@ class Element:
     #     types = {"xpath": By.XPATH, "css": By.CSS_SELECTOR}
     #     self.selectors.insert(index, [selector, types[sel_type]])
 
-
     def __str__(self):
         return str(self.selectors)
         # return '| {:^9}| {:<22}| {:<15}| {}'.format(*self.status_row.values())
+
 
 class Selector:
     def __init__(self, sel_value: str, sel_type=None):
@@ -421,7 +507,7 @@ class Selector:
             self.sel_type = sel_type
         else:
             # Auto-detect the selector type
-            #TODO: this is fragile
+            # TODO: this is fragile
             if sel_value[0] == "/":
                 self.sel_type = By.XPATH
             else:
@@ -430,6 +516,6 @@ class Selector:
     # Selector[0] = self.sel_value, Selector[1] = self.sel_type
     def __iter__(self):
         return iter([self.sel_value, self.sel_type])
-    
+
     def __str__(self):
-        return f"{self.sel_value} (type \"{self.sel_type}\")"
+        return f'{self.sel_value} (type "{self.sel_type}")'
